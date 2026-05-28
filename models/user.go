aa816983ddc6807da8b68b0888a20b4925ff92bf
Yannick <yannickkamdemkouam@yahoo.fr>
2026-05-28 15:02:07 +0200

    Additional attributes in the user table

--- models/user.go
+++ models/user.go
@@ -1,18 +1,38 @@
 package models
 
 import (
+	"time"
+
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
+	// Champs existants
 	Username string `gorm:"unique;not null" json:"username"`
 	Email    string `gorm:"unique;not null" json:"email"`
-	Password string `json:"-"`
+	Password string `json:"-"` // jamais exposé en JSON
+
+	// Nouveaux champs nullables (pointeurs)
+	SocketId           *string   `json:"socketId,omitempty"`                     // nul à la création
+	FreeChipsAmount    *float64  `json:"freeChipsAmount,omitempty"`              // entier avec virgule, nul
+	RealChipsAmount    *float64  `json:"realChipsAmount,omitempty"`              // entier avec virgule, nul
+	ProfilePictureLink *string   `json:"profilePictureLink,omitempty"`           // nul
+	LastModification   time.Time `gorm:"autoUpdateTime" json:"lastModification"` // mis à jour automatiquement
+
+	// Clés étrangères pour les relations (valeurs nulles tant que non définies)
+	PlayingTableID    *uint `json:"playingTableId,omitempty"`
+	PersonalDetailsID *uint `json:"personalDetailsId,omitempty"`
+	PaymentDetailsID  *uint `json:"paymentDetailsId,omitempty"`
+
+	// Relations (à décommenter quand les modèles existent)
+	// PlayingTable      *PlayingTable   `gorm:"foreignKey:PlayingTableID"`
+	// PersonalDetails   *PersonalDetail `gorm:"foreignKey:PersonalDetailsID"`
+	// PaymentDetails    *PaymentDetail  `gorm:"foreignKey:PaymentDetailsID"`
 }
 
-// HashPassword hashe le mot de passe avant la création
+// HashPassword hashe le mot de passe
 func (user *User) HashPassword(password string) error {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	if err != nil {
@@ -24,6 +44,5 @@ func (user *User) HashPassword(password string) error {
 
 // CheckPassword vérifie le mot de passe
 func (user *User) CheckPassword(providedPassword string) error {
-	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(providedPassword))
-	return err
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(providedPassword))
 }
